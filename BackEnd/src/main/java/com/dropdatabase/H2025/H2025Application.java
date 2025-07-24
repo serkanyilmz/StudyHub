f0734d45384d726aa5ccad29e890a4d797144ca1
Serkan Yilmaz <serkan.2003.yilmaz@gmail.com>
2025-07-24 11:23:48 +0300

    DD-1 Spring has been initialized

---
+++ BackEnd/src/main/java/com/dropdatabase/H2025/H2025Application.java
@@ -0,0 +1,13 @@
+package com.dropdatabase.H2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2025Application.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/dropdatabase/H2025/H2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropdatabase.H2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
